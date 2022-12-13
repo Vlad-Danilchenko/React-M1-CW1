fc97798b4c7f887d5f13eb29af57bf95643518ee
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2022-12-13 16:33:47 +0200

    add les 1

---
+++ src/components/Recipe.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { BsAlarm } from 'react-icons/bs';
+import { HiOutlineChartPie, HiOutlineChartBar } from 'react-icons/hi';
+
+export const Recipe = ({ recipe: { name, time, servings, calories } }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <div>
+        <div>
+          <BsAlarm />
+          {time} min
+        </div>
+        <div>
+          <HiOutlineChartPie />
+          {servings} servings
+        </div>
+        <div>
+          <HiOutlineChartBar />
+          {calories} calories
+        </div>
+      </div>
+      <div>
+        <h3>Difficulty</h3>
+        <span>Easy</span>
+        <span>Medium</span>
+        <span>Hard</span>
+      </div>
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  recipe: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    servings: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+  }).isRequired,
+};
